d1765b8ccd7f3123c0531b1d2b92d2d1500ad3d4
Lee Gillentine <lee@fishburd.com>
2012-12-21 20:04:31 -0500

    Working out adding compass and sass

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@ require 'rubygems'
 require 'sinatra'
 require 'mongo'
 require 'json'
+require 'haml'
+require 'sass'
+require 'compass'
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = File.join "views", "stylesheets"
+  end
+
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf8'
+  sass (:"stylesheets/#{params[:name]}")
+end
+
 
 #DB = Mongo::Connection.new.db("case", :pool_size => 5, :timeout => 5)
 
