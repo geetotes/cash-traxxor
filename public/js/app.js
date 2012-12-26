80b744c157984dea2d16432280f21699d2e2b5ce
Lee Gillentine <lee@fishburd.com>
2012-12-25 19:19:18 -0500

    Working out dates for income

--- public/js/app.js
+++ public/js/app.js
@@ -19,22 +19,63 @@ $(function(){
   }
   });
 
-    window.GraphView = Backbone.View.extend({
-        el: $("#viz"),
 
-        lostChart: function(){
-            var lost = [4, 8, 15, 16, 23, 42], //LOST
-                cash = [],
-                spend = [],
-                i;
-      for(i = 0; i < lost.length; i++) {
-        spendDate = (new Date).getTime();
-        spend.push({ x: spendDate, y: 30});
-        var cashDate = new Date(2012,12,i).getTime();
-        cash.push({ x: cashDate, y: lost[i]});
+
+  window.CashModel = Backbone.Model.extend({
+    defaults: {
+      "income": "true",
+      "amount": "100",
+      "frequency": "4"//should be a number indicating how often it happens in a month
+    },
+    calculateFrequency: function(){
+      //do something
+    },
+    generateData: function(){
+      var now = new Date(),
+          x,
+          y,
+          generatedData = [];
+
+      var days = new Date(now.getYear(), now.getMonth()).getDate();
+      var daysIncrement = Math.floor(days/this.frequency);
+
+      for(var i = 1; i <= this.frequency; i++)
+      {
+        generatedData.push({x: new Date(now.getYear(), now.getMonth(), i * daysIncrement).getTime(), y: (i * this.amount)});
+        //TODO: debug here
+
       }
-      return [{ values: cash, key: "cash", color: "#2ca02c"},
-              { values: spend, area: true, key: "spending", color:"lightsalmon"}];
+      //based on this.amount, this.income, and this.frequency, this will generate the x/y data
+      return generatedData;
+    }
+  });
+
+  window.CashModels = Backbone.Collection.extend({
+    model: CashModel //TODO: add url attrib
+  });
+
+  window.GraphView = Backbone.View.extend({
+      el: $("#viz"),
+
+      lostChart: function(){
+          var lost = [4, 8, 15, 16, 23, 42], //LOST
+              cash = [],
+              spend = [],
+              i,
+              cashModel = new CashModel;
+    for(i = 0; i < lost.length; i++) {
+      spendDate = (new Date).getTime();
+      spend.push({ x: spendDate, y: 30});
+      var cashDate = new Date(2012,12,i).getTime();
+      cash.push({ x: cashDate, y: lost[i]});
+    }
+
+    cashModel.amount = "342";
+    cashModel.income = true;
+    cashModel.frequency = "4";
+
+    return [{ values: cashModel.generateData(), key: "cash", color: "#2ca02c"},
+            { values: spend, area: true, key: "spending", color:"lightsalmon"}];
 
     },
     chart: function(){
@@ -43,34 +84,34 @@ $(function(){
           minDate = "20111201",
           maxDate = "20121201";
 
-  chart.x(function(d,i){return i});
+    chart.x(function(d,i){return i});
 
-  chart.xAxis
-    .axisLabel('Time')
-    .tickFormat(function(d){
-      var dx = data[0].values[d] && data[0].values[d].x || 0;
-      return d3.time.format('%x')(new Date(dx))
+    chart.xAxis
+      .axisLabel('Time')
+      .tickFormat(function(d){
+        var dx = data[0].values[d] && data[0].values[d].x || 0;
+        return d3.time.format('%x')(new Date(dx))
+      });
+
+
+    chart.yAxis
+      .axisLabel('Money')
+      .tickFormat(d3.format('.02f'));
+
+    d3.select("#viz svg")
+      .datum(data)
+      .transition().duration(500)
+      .call(chart);
+
+    nv.utils.windowResize(function() { d3.select("#viz svg").call(chart)});
+
+    return chart;
+      },
+
+      render: function(){
+        nv.addGraph(this.chart());
+      }
     });
-
-
-  chart.yAxis
-    .axisLabel('Money')
-    .tickFormat(d3.format('.02f'));
-
-  d3.select("#viz svg")
-    .datum(data)
-    .transition().duration(500)
-    .call(chart);
-
-  nv.utils.windowResize(function() { d3.select("#viz svg").call(chart)});
-
-  return chart;
-    },
-
-    render: function(){
-      nv.addGraph(this.chart());
-    }
-  });
 
   window.app = new AppView;
 
